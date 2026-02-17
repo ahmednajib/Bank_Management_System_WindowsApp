96246e0e19b777bd3d4de3cb80c1e0e4e089bdf8
ahmednajib <ahmdaldyny3@gmail.com>
2026-02-17 16:47:02 +0300

    added Filter for Clients List

--- BankManagement/Clients/ManageClients/frmManageClients.Designer.cs
+++ BankManagement/Clients/ManageClients/frmManageClients.Designer.cs
@@ -29,10 +29,11 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.topPanel = new Guna.UI2.WinForms.Guna2Panel();
+            this.cmbIsActive = new Guna.UI2.WinForms.Guna2ComboBox();
             this.cmbFilterBy = new Guna.UI2.WinForms.Guna2ComboBox();
             this.txtFilter = new Guna.UI2.WinForms.Guna2TextBox();
             this.lblFindBy = new Guna.UI2.WinForms.Guna2HtmlLabel();
@@ -51,7 +52,6 @@
             this.bottomPanel = new Guna.UI2.WinForms.Guna2Panel();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
             this.frmClientManagement = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
-            this.cmbActiveOrNot = new Guna.UI2.WinForms.Guna2ComboBox();
             this.topPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvClients)).BeginInit();
             this.guna2ContextMenuStrip1.SuspendLayout();
@@ -61,8 +61,8 @@
             // 
             // topPanel
             // 
-            this.topPanel.BackColor = System.Drawing.Color.Transparent;
-            this.topPanel.Controls.Add(this.cmbActiveOrNot);
+            this.topPanel.BackColor = System.Drawing.Color.Silver;
+            this.topPanel.Controls.Add(this.cmbIsActive);
             this.topPanel.Controls.Add(this.cmbFilterBy);
             this.topPanel.Controls.Add(this.txtFilter);
             this.topPanel.Controls.Add(this.lblFindBy);
@@ -72,6 +72,29 @@
             this.topPanel.Name = "topPanel";
             this.topPanel.Size = new System.Drawing.Size(1141, 129);
             this.topPanel.TabIndex = 0;
+            // 
+            // cmbIsActive
+            // 
+            this.cmbIsActive.BackColor = System.Drawing.Color.Transparent;
+            this.cmbIsActive.BorderRadius = 10;
+            this.cmbIsActive.BorderThickness = 2;
+            this.cmbIsActive.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cmbIsActive.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbIsActive.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cmbIsActive.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cmbIsActive.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.cmbIsActive.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.cmbIsActive.ItemHeight = 20;
+            this.cmbIsActive.Items.AddRange(new object[] {
+            "All",
+            "Yes",
+            "No"});
+            this.cmbIsActive.Location = new System.Drawing.Point(270, 96);
+            this.cmbIsActive.Name = "cmbIsActive";
+            this.cmbIsActive.Size = new System.Drawing.Size(131, 26);
+            this.cmbIsActive.TabIndex = 4;
+            this.cmbIsActive.Visible = false;
+            this.cmbIsActive.SelectedIndexChanged += new System.EventHandler(this.cmbActiveOrNot_SelectedIndexChanged);
             // 
             // cmbFilterBy
             // 
@@ -91,12 +114,13 @@
             "Full Name",
             "National No",
             "Nationality",
-            "Is Active",
-            "Gender"});
+            "Gender",
+            "Is Active"});
             this.cmbFilterBy.Location = new System.Drawing.Point(84, 96);
             this.cmbFilterBy.Name = "cmbFilterBy";
             this.cmbFilterBy.Size = new System.Drawing.Size(176, 26);
             this.cmbFilterBy.TabIndex = 3;
+            this.cmbFilterBy.SelectedIndexChanged += new System.EventHandler(this.cmbFilterBy_SelectedIndexChanged);
             // 
             // txtFilter
             // 
@@ -118,6 +142,7 @@
             this.txtFilter.Size = new System.Drawing.Size(213, 27);
             this.txtFilter.TabIndex = 2;
             this.txtFilter.TextChanged += new System.EventHandler(this.txtFilter_TextChanged);
+            this.txtFilter.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtFilter_KeyPress);
             // 
             // lblFindBy
             // 
@@ -167,27 +192,29 @@
             this.dgvClients.AllowUserToOrderColumns = true;
             this.dgvClients.AllowUserToResizeColumns = false;
             this.dgvClients.AllowUserToResizeRows = false;
-            dataGridViewCellStyle4.BackColor = System.Drawing.Color.White;
-            this.dgvClients.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle4;
-            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle5.Font = new System.Drawing.Font("Tahoma", 8F);
-            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvClients.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
-            this.dgvClients.ColumnHeadersHeight = 4;
-            this.dgvClients.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            this.dgvClients.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvClients.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.None;
+            this.dgvClients.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dgvClients.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableAlwaysIncludeHeaderText;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Tahoma", 8F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvClients.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvClients.ColumnHeadersHeight = 30;
             this.dgvClients.ContextMenuStrip = this.guna2ContextMenuStrip1;
-            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle6.BackColor = System.Drawing.Color.White;
-            dataGridViewCellStyle6.Font = new System.Drawing.Font("Tahoma", 8F);
-            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
-            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
-            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgvClients.DefaultCellStyle = dataGridViewCellStyle6;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Tahoma", 8F);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvClients.DefaultCellStyle = dataGridViewCellStyle3;
             this.dgvClients.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgvClients.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
             this.dgvClients.Location = new System.Drawing.Point(0, 129);
@@ -195,6 +222,7 @@
             this.dgvClients.Name = "dgvClients";
             this.dgvClients.ReadOnly = true;
             this.dgvClients.RowHeadersVisible = false;
+            this.dgvClients.RowHeadersWidth = 80;
             this.dgvClients.Size = new System.Drawing.Size(1141, 614);
             this.dgvClients.TabIndex = 6;
             this.dgvClients.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
@@ -202,14 +230,14 @@
             this.dgvClients.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
             this.dgvClients.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
             this.dgvClients.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
-            this.dgvClients.ThemeStyle.BackColor = System.Drawing.Color.White;
+            this.dgvClients.ThemeStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
             this.dgvClients.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
             this.dgvClients.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
             this.dgvClients.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
             this.dgvClients.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Tahoma", 8F);
             this.dgvClients.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
-            this.dgvClients.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
-            this.dgvClients.ThemeStyle.HeaderStyle.Height = 4;
+            this.dgvClients.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dgvClients.ThemeStyle.HeaderStyle.Height = 30;
             this.dgvClients.ThemeStyle.ReadOnly = true;
             this.dgvClients.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
             this.dgvClients.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
@@ -218,7 +246,6 @@
             this.dgvClients.ThemeStyle.RowsStyle.Height = 22;
             this.dgvClients.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
             this.dgvClients.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
-            this.dgvClients.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dgvClients_CellFormatting);
             // 
             // guna2ContextMenuStrip1
             // 
@@ -288,7 +315,7 @@
             // 
             // bottomPanel
             // 
-            this.bottomPanel.BackColor = System.Drawing.Color.Transparent;
+            this.bottomPanel.BackColor = System.Drawing.Color.Silver;
             this.bottomPanel.Controls.Add(this.guna2HtmlLabel1);
             this.bottomPanel.Controls.Add(this.lblNumberOfRecords);
             this.bottomPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
@@ -303,32 +330,13 @@
             // 
             // frmClientManagement
             // 
-            this.frmClientManagement.BorderRadius = 30;
+            this.frmClientManagement.AnimationType = Guna.UI2.WinForms.Guna2BorderlessForm.AnimateWindowType.AW_VER_POSITIVE;
+            this.frmClientManagement.BorderRadius = 20;
             this.frmClientManagement.ContainerControl = this;
             this.frmClientManagement.DockIndicatorTransparencyValue = 0.6D;
+            this.frmClientManagement.HasFormShadow = false;
+            this.frmClientManagement.ShadowColor = System.Drawing.Color.Navy;
             this.frmClientManagement.TransparentWhileDrag = true;
-            // 
-            // cmbActiveOrNot
-            // 
-            this.cmbActiveOrNot.BackColor = System.Drawing.Color.Transparent;
-            this.cmbActiveOrNot.BorderRadius = 10;
-            this.cmbActiveOrNot.BorderThickness = 2;
-            this.cmbActiveOrNot.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
-            this.cmbActiveOrNot.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cmbActiveOrNot.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.cmbActiveOrNot.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.cmbActiveOrNot.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.cmbActiveOrNot.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
-            this.cmbActiveOrNot.ItemHeight = 20;
-            this.cmbActiveOrNot.Items.AddRange(new object[] {
-            "All",
-            "Active",
-            "Not Active"});
-            this.cmbActiveOrNot.Location = new System.Drawing.Point(270, 96);
-            this.cmbActiveOrNot.Name = "cmbActiveOrNot";
-            this.cmbActiveOrNot.Size = new System.Drawing.Size(131, 26);
-            this.cmbActiveOrNot.TabIndex = 4;
-            this.cmbActiveOrNot.Visible = false;
             // 
             // frmManageClients
             // 
@@ -376,6 +384,6 @@
         private System.Windows.Forms.ToolStripMenuItem addAnAccountToolStripMenuItem;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private Guna.UI2.WinForms.Guna2BorderlessForm frmClientManagement;
-        private Guna.UI2.WinForms.Guna2ComboBox cmbActiveOrNot;
+        private Guna.UI2.WinForms.Guna2ComboBox cmbIsActive;
     }
 }
--- BankManagement/Clients/ManageClients/frmManageClients.cs
+++ BankManagement/Clients/ManageClients/frmManageClients.cs
@@ -1,9 +1,8 @@
 ﻿using BLL_BankManagement;
 using System;
 using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace BankManagement.Clients.ManageClients
 {
@@ -23,29 +22,68 @@ namespace BankManagement.Clients.ManageClients
             InitializeComponent();
         }
 
-        private void dgvClients_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
-        {
-            // Check if the column is the "IsActive" column (adjust index or name as needed)
-            // And check if the value is false (inactive)
-            if (dgvClients.Columns[e.ColumnIndex].Name == "IsActive")
-            {
-                // Color the entire row light red to indicate inactive status
-                dgvClients.Rows[e.RowIndex].DefaultCellStyle.BackColor = Color.FromArgb(255, 200, 200);
-                dgvClients.Rows[e.RowIndex].DefaultCellStyle.ForeColor = Color.Black;
-            }
-        }
-
         private void frmManageClients_Load(object sender, EventArgs e)
         {
             _LoadPeopleData();
+
+            dgvClients.Columns[0].HeaderText = "Client ID";
+            dgvClients.Columns[0].Width = 100;
+
+            dgvClients.Columns[1].HeaderText = "National No";
+            dgvClients.Columns[1].Width = 100;
+
+            dgvClients.Columns[2].HeaderText = "Full Name";
+            dgvClients.Columns[2].Width = 150;
+
+            dgvClients.Columns[3].HeaderText = "Birth Date";
+            dgvClients.Columns[3].Width = 150;
+
+            dgvClients.Columns[4].HeaderText = "Gender";
+            dgvClients.Columns[4].Width = 110;
+
+            dgvClients.Columns[5].HeaderText = "Phone";
+            dgvClients.Columns[5].Width = 140;
+
+            dgvClients.Columns[6].HeaderText = "Email";
+            dgvClients.Columns[6].Width = 160;
+
+            dgvClients.Columns[7].HeaderText = "Nationality";
+            dgvClients.Columns[7].Width = 120;
+
+            dgvClients.Columns[8].HeaderText = "Active status";
+            dgvClients.Columns[8].Width = 110;
         }
 
         private void txtFilter_TextChanged(object sender, EventArgs e)
         {
             string FilterColumn = "";
 
+            switch (cmbFilterBy.Text)
+            {
+                case "Client ID":
+                    FilterColumn = "ClientID";
+                    break;
 
+                case "National No":
+                    FilterColumn = "NationalNo";
+                    break;
 
+                case "Full Name":
+                    FilterColumn = "FullName";
+                    break;
+
+                case "Nationality":
+                    FilterColumn = "Nationality";
+                    break;
+
+                case "Gender":
+                    FilterColumn = "Gender";
+                    break;
+
+                default:
+                    FilterColumn = "None";
+                    break;
+            }
             //Reset the filters in case nothing selected or filter value conains nothing.
             if (txtFilter.Text.Trim() == "" || FilterColumn == "None")
             {
@@ -60,7 +98,88 @@ namespace BankManagement.Clients.ManageClients
             else
                 _dtAllClients.DefaultView.RowFilter = string.Format("[{0}] LIKE '{1}%'", FilterColumn, txtFilter.Text.Trim());
 
+            lblNumberOfRecords.Text = _dtAllClients.DefaultView.Count.ToString();
+        }
+
+        private void cmbFilterBy_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Reset the filter value textbox and visibility based on the selected filter
+            _dtAllClients.DefaultView.RowFilter = "";
             lblNumberOfRecords.Text = dgvClients.Rows.Count.ToString();
+
+            if (cmbFilterBy.Text == "Is Active")
+            {
+                txtFilter.Visible = false;
+                cmbIsActive.Visible = true;
+                cmbIsActive.Focus();
+                cmbIsActive.SelectedIndex = 0;
+            }
+            else
+            {
+                txtFilter.Visible = (cmbFilterBy.Text != "None");
+                cmbIsActive.Visible = false;
+                txtFilter.Text = "";
+                txtFilter.Focus();
+            }
+        }
+
+        private void txtFilter_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string selectedFilter = cmbFilterBy.SelectedItem?.ToString();
+
+            if (string.IsNullOrEmpty(selectedFilter))
+                return;
+
+            char keyChar = e.KeyChar;
+
+            // Allow control characters like Backspace
+            if (char.IsControl(keyChar))
+                return;
+
+            switch (selectedFilter)
+            {
+                case "Client ID":
+                    // Allow only digits
+                    if (!char.IsDigit(keyChar))
+                    {
+                        e.Handled = true;
+                    }
+                    break;
+
+                case "Full Name":
+                case "Nationality":
+                case "Gender":
+                    // Allow only letters
+                    if (!char.IsLetter(keyChar))
+                    {
+                        e.Handled = true;
+                    }
+                    break;
+
+                case "National No":
+                    // Allow letters, digits, and common email characters
+                    if (!char.IsLetterOrDigit(keyChar) && keyChar != '@' && keyChar != '.' && keyChar != '-' && keyChar != '_')
+                    {
+                        e.Handled = true;
+                    }
+                    break;
+            }
+        }
+
+        private void cmbActiveOrNot_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbIsActive.Text == "Yes")
+                // Use single quotes because the View returns a string "Active"
+                _dtAllClients.DefaultView.RowFilter = "IsActive = 'Active'";
+            else if (cmbIsActive.Text == "No")
+                // Use single quotes because the View returns a string "Not Active"
+                _dtAllClients.DefaultView.RowFilter = "IsActive = 'Not Active'";
+            else
+                // Clear the filter
+                _dtAllClients.DefaultView.RowFilter = "";
+
+            // Best practice: Count from the View, not the Grid rows, for accuracy
+            lblNumberOfRecords.Text = _dtAllClients.DefaultView.Count.ToString();
         }
     }
 }
